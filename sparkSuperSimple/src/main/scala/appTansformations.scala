c371e083e99c6544ae28bdb5d6cfd0b7c0e6876c
Charlie Zha <charlie.zha@ericsson.com>
2017-06-15 15:38:56 +0800

    finish transformation

--- sparkSuperSimple/src/main/scala/appTansformations.scala
+++ sparkSuperSimple/src/main/scala/appTansformations.scala
@@ -5,9 +5,10 @@ import org.apache.spark.{SparkConf, SparkContext}
  */
 object appTansformations {
   def main(args: Array[String]) {
-    val logFile = getClass().getResource("readme.txt").getPath // Should be some file on your system
     val conf = new SparkConf().setAppName("Simple Application").setMaster("local")
     val sc = new SparkContext(conf)
+
+    val logFile = getClass().getResource("readme.txt").getPath // Should be some file on your system
     val logData = sc.textFile(logFile, 2).cache()
 
     printResult("rows", logData.map(line => "hello" + line).collect())
@@ -29,7 +30,8 @@ object appTansformations {
     /**
      * collect the first element of this partition
      */
-    printResult("flatMapPartitionRet",  logData.mapPartitions(lines => List(lines.next).iterator).collect())
+    printResult("flatMapPartitionRet", logData.mapPartitions(lines => List(lines.next).iterator).collect())
+
     /**
      * Collect all element appending index
      */
@@ -45,18 +47,40 @@ object appTansformations {
     printResult("distinct", par1.union(par2).distinct.collect)
 
 
+    val userFile = getClass().getResource("user.txt").getPath // Should be some file on your system
+    val user = sc.textFile(userFile, 2).cache()
+    val rows = user.map(line => line.split(","))
+    val jobToName = rows.map(line => (line(3), line(0)))
+
+    printResult("groupBykey", jobToName.groupByKey.collect)
+    printResult("reduceByKey", jobToName.reduceByKey((v1,v2) => v1 + " and " + v2 ).collect)
+    // skip aggregateByKey because I haven't understand what it used for
+    //printResult("reduceByKey", jobToName.aggregateByKey((v1,v2) => v1 + " and " + v2 ).collect)
+    printResult("sortByKey",jobToName.sortByKey().collect())
+
+
+    val addressFile = getClass().getResource("address.txt").getPath // Should be some file on your system
+    val address = sc.textFile(addressFile, 2).cache()
+    val rowsUser = user.map(line => line.split(","))
+    val nameToInfo = rowsUser.map(line => (line(0), (line(1),line(2),line(3))))
+    val rowAddress = address.map(line => line.split(","))
+    val nameToAddress = rowAddress.map(line=>(line(0),line(1)))
+    printResult("join",nameToInfo.join(nameToAddress).collect())
+
+
+    val rdd1 = sc.parallelize(List("abe", "abby", "apple"))
+    val rdd2 = sc.parallelize(List("apple", "beatty", "beatrice"))
+    printResult("cartisian",rdd1.cartesian(rdd2).collect())
+    
     sc.stop()
   }
 
-  def printResult(name: String, rows: Array[String]) {
+  def printResult[A,B](name: A, rows: Array[B]) {
     println(s"$name=")
     rows.foreach(row => print(row + ","))
     println("")
   }
 
-  def printResult(name: String, rows: Array[Int]) {
-    println(s"$name=")
-    rows.foreach(row => print(row + ","))
-    println("")
-  }
+
+
 }
